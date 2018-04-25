236c6dbd4502965393001b9afb43a5c9d9071314
danielfsokol <danielfsokol@gmail.com>
2018-04-24 22:01:44 -0400

    completed delete

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -4,13 +4,24 @@ class Api::ListsController < ApiController
    def create
      list = List.new(list_params)
      list.user_id = params[:user_id]
-     
+
      if list.save
        render json: list
      else
        render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
      end
    end
+
+   def destroy
+     begin
+       list = List.find(params[:id])
+       list.destroy
+       render json: {}, status: :no_content
+     rescue ActiveRecord::RecordNotFound
+       render :json => {}, :status => :not_found
+     end
+   end
+
 
 
    private
